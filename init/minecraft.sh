e018974aeacde5db7378098eaef99978706420c1
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-06 22:17:31 -0400

    Try Paper

--- init/minecraft.sh
+++ init/minecraft.sh
@@ -15,7 +15,7 @@ rm ngrok.zip
 echo "done"
 
 echo -n "-----> Installing Minecraft..."
-minecraft_url="https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.16.3.jar"
+minecraft_url="https://papermc.io/api/v1/paper/1.16.3/216/download"
 curl -o minecraft.jar -s -L $minecraft_url
 echo "done"
 
