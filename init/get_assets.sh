8d10c0b45f5f99c3fc635a5edebd32e3e4890954
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 16:58:43 -0400

    sync you drive me nuts

--- init/get_assets.sh
+++ init/get_assets.sh
@@ -2,15 +2,12 @@
 set -eu
 
 echo "Pulling directories from S3"
-aws s3 sync "s3://$AWS_BUCKET" . \
-  --only-show-errors \
-  --exclude "*" \
-  --include "world/*" \
-  --include "world_nether/*" \
-  --include "world_the_end/*" \
-  --include "plugins/*" \
-  --include "cache/*" \
-  --include "logs/*"
+aws s3 sync "s3://$AWS_BUCKET" /app --dryrun --only-show-errors --exclude "*" --include "world/*"
+  # --include "world_nether/*" \
+  # --include "world_the_end/*" \
+  # --include "plugins/*" \
+  # --include "cache/*" \
+  # --include "logs/*"
 echo "Completed S3 directory pull"
 
 exit
