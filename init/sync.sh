b8e2bcfd0f24e82e76863d2a86df8b93ad292ffa
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 09:56:16 -0400

    Fix sync script

--- init/sync.sh
+++ init/sync.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+get_if_does_not_exist() {
+  if [ ! -d $1 ] then
+    mkdir -p $1
+    aws s3 cp --recursive s3://$AWS_BUCKET/$1 $1
+  fi
+}
+
 if [ -n "$AWS_BUCKET" ]; then
   cat << EOF > .s3cfg
 [default]
@@ -7,16 +14,9 @@ access_key = ${AWS_ACCESS_KEY}
 secret_key = ${AWS_SECRET_KEY}
 EOF
 
-  if aws s3 ls s3://$AWS_BUCKET 2>&1 | grep -q 'NoSuchBucket' then
+  if aws s3 ls s3://$AWS_BUCKET 2>&1 | grep -q 'NoSuchBucket'; then
     aws s3 mb s3://$AWS_BUCKET
   fi
-
-  get_if_does_not_exist() {
-    if [ ! -d $1 ] then
-      mkdir -p $1
-      aws s3 cp --recursive s3://$AWS_BUCKET/$1 $1
-    fi
-  }
 
   get_if_does_not_exist world
   get_if_does_not_exist world_nether
