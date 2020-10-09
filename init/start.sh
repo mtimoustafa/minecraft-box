0032fc047580b37263f443c6ac91852f54f0fbb9
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 09:44:18 -0400

    Remove sudo

--- init/start.sh
+++ init/start.sh
@@ -9,8 +9,8 @@ fi
 echo -n "-----> Installing aws-cli..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
 rm awscliv2.zip
+./aws/install
 echo "done"
 
 echo -n "-----> Installing ngrok..."
