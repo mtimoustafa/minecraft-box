761fded376f00a4965b7f3d69884c4161af0fe5b
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 18:49:24 -0400

    so sus

--- init/get_assets.sh
+++ init/get_assets.sh
@@ -2,12 +2,13 @@
 set -eu
 
 echo "Pulling directories from S3"
-aws s3 sync "s3://$AWS_BUCKET" . --dryrun --only-show-errors --exclude "\*" --include "world/*"
-  # --include "world_nether/*" \
-  # --include "world_the_end/*" \
-  # --include "plugins/*" \
-  # --include "cache/*" \
-  # --include "logs/*"
+aws s3 sync "s3://$AWS_BUCKET" ./ --dryrun --only-show-errors --exclude "*" \
+  --include "world/*" \
+  --include "world_nether/*" \
+  --include "world_the_end/*" \
+  --include "plugins/*" \
+  --include "cache/*" \
+  --include "logs/*"
 echo "Completed S3 directory pull"
 
 exit
