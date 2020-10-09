e8517268198386f48530e3f01885a0a269f94fd5
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 16:33:51 -0400

    Try fetching files first

--- init/sync.sh
+++ init/sync.sh
@@ -21,24 +21,36 @@ if [ -n "$AWS_BUCKET" ]; then
     aws s3 mb "s3://$AWS_BUCKET"
   fi
 
-  get_if_does_not_exist \
-    world \
-    world_nether \
-    world_the_end \
-    plugins \
-    cache \
-    logs
+  echo "Pulling directories from S3"
+  aws s3 sync "s3://$AWS_BUCKET" . \
+    --dryrun \
+    --exclude "*" \
+    --include "world/*" \
+    --include "world_nether/*" \
+    --include "world_the_end/*" \
+    --include "plugins/*" \
+    --include "cache/*" \
+    --include "logs/*"
+  echo "Completed S3 directory pull"
 
-  echo "Syncing to S3"
-  ls -lah .
-  pwd
-  aws s3 sync /app "s3://$AWS_BUCKET" --dryrun --exclude "*" --include "world/*"
-    # --include "world_nether/*" \
-    # --include "world_the_end/*" \
-    # --include "plugins/*" \
-    # --include "cache/*" \
-    # --include "logs/*"
-  echo "Sync completed"
+  # get_if_does_not_exist \
+  #   world \
+  #   world_nether \
+  #   world_the_end \
+  #   plugins \
+  #   cache \
+  #   logs
+
+  # echo "Syncing to S3"
+  # ls -lah .
+  # pwd
+  # aws s3 sync /app "s3://$AWS_BUCKET" --dryrun --exclude "*" --include "world/*"
+  #   # --include "world_nether/*" \
+  #   # --include "world_the_end/*" \
+  #   # --include "plugins/*" \
+  #   # --include "cache/*" \
+  #   # --include "logs/*"
+  # echo "Sync completed"
 else
   echo "Failed to sync: could not find $AWS_BUCKET"
 fi
