d00f2c152e4628d580c76a6d3a04ab9de43694cd
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 10:26:39 -0400

    Remove s3cmd cfg file creation + improve check function

--- init/sync.sh
+++ init/sync.sh
@@ -2,29 +2,26 @@
 set -eu
 
 get_if_does_not_exist() {
-  if [ ! -d "$1" ]; then
-    mkdir -p "$1"
-    aws s3 cp --recursive "s3://$AWS_BUCKET/$1" "$1"
-  fi
+  for dir in "$@"; do
+    if [ ! -d "$dir" ]; then
+      mkdir -p "$dir"
+      aws s3 cp --recursive "s3://$AWS_BUCKET/$dir" "$dir"
+    fi
+  done
 }
 
 if [ -n "$AWS_BUCKET" ]; then
-  cat << EOF > .s3cfg
-[default]
-access_key = ${AWS_ACCESS_KEY}
-secret_key = ${AWS_SECRET_KEY}
-EOF
-
   if aws s3 ls "s3://$AWS_BUCKET" 2>&1 | grep -q 'NoSuchBucket'; then
     aws s3 mb "s3://$AWS_BUCKET"
   fi
 
-  get_if_does_not_exist world
-  get_if_does_not_exist world_nether
-  get_if_does_not_exist world_the_end
-  get_if_does_not_exist plugins
-  get_if_does_not_exist cache
-  get_if_does_not_exist logs
+  get_if_does_not_exist \
+    world \
+    world_nether \
+    world_the_end \
+    plugins \
+    cache \
+    logs
 
   aws s3 sync . "s3://$AWS_BUCKET" --recursive \
     --exclude "*" \
