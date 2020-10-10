bdf58f6decc17e4071056ed914904c7b047e5523
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-09 20:53:00 -0400

    Actually fix routing

--- init/web.sh
+++ init/web.sh
@@ -2,8 +2,4 @@
 set -eu
 
 port=${1:-${PORT:-8080}}
-
-# Get to root directory
-cd /
-
 eval "ruby -rwebrick -e'WEBrick::HTTPServer.new(:BindAddress => \"0.0.0.0\", :Port => $port, :MimeTypes => {\"rhtml\" => \"text/html\"}, :DocumentRoot => Dir.pwd).start'"
