66d67d2ce3f66979270318634617da2dc38a6b77
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-11 14:04:11 -0400

    Add argument

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-aws ecs update-service --cluster minecraft-box-cluster --service minecraft-box-service --force-new-deployment
+#!/bin/sh
+aws ecs update-service \
+  --force-new-deployment \
+  --cluster minecraft-box-cluster \
+  --service minecraft-box-service \
+  --region us-east-2
