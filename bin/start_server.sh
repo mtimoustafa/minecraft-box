ddba73f98a1d1dcf68aa9b04bc3689cc147e1e53
Mohamed Moustafa <mohamed.moustafa@vidyard.com>
2020-10-15 20:59:58 -0400

    Add redundancy to start script

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -16,8 +16,8 @@ if [ ! -d "minecraft" ]; then
   echo "[INIT] Warning: minecraft directory doesn't exist; creating"
   mkdir minecraft
 fi
-cp -R minecraft-properties/. minecraft/.
-cp -R plugins/. minecraft/plugins/.
+mkdir -p minecraft-properties && cp -R minecraft-properties/. minecraft/.
+mkdir -p plugins && cp -R plugins/. minecraft/plugins/.
 cd minecraft
 
 echo "[INIT] Installing Minecraft"
